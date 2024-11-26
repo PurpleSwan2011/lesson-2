f5ac758cdef9fdaae89729080a14a99958f000ac
Saima Fathima <saima.fathi2011@gmail.com>
2024-11-26 16:47:46 +0530

    act2

---
+++ act2.py
@@ -0,0 +1,10 @@
+#creating different variables
+x = 4
+y = "Saima"
+print(x)
+print(y)
+codingal="codingal"
+print(codingal)
+#user input
+name=input("enter your name:")
+print("\nHello", name, "\nwelcome to codingal")
