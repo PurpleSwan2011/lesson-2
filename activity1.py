82f3282253354628fa9ffd68f727f37499845fda
Saima Fathima <saima.fathi2011@gmail.com>
2024-11-26 16:39:57 +0530

    activity1

---
+++ activity1.py
@@ -0,0 +1,14 @@
+#print a sentence
+print("Welcome to codingal")
+
+#print a number
+print(4)
+
+#next line after print
+print("hello python \n")
+
+#pass multiple arguements for printing
+print("hello",4)
+
+#end arguement of print statement
+print("welcome to",end = "*")
